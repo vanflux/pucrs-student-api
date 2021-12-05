7bb9c2edb0177f0f6b0c011768d7b5f69cacea3b
vanflux <lucasabruzzi@hotmail.com>
2021-12-05 17:26:25 -0300

    Clean script, noImplicitAny, index.js and index.d.ts, exporting types package.json

---
+++ dist/index.d.ts
@@ -0,0 +1,1 @@
+export { PucrsClient } from "./client";
---
+++ dist/index.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.PucrsClient = void 0;
+var client_1 = require("./client");
+Object.defineProperty(exports, "PucrsClient", { enumerable: true, get: function () { return client_1.PucrsClient; } });
