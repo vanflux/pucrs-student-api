61e86486ada18212cc576ce143bbc1e4faaf150f
vanflux <lucasabruzzi@hotmail.com>
2021-12-05 17:33:00 -0300

    Exporting more types

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,11 @@
 export { PucrsClient } from "./client";
+
+// Load client plugins
 import "./plugins/hours-grid.plugin";
 import "./plugins/grade-grid.plugin";
 import "./plugins/bill.plugin";
+
+// Export parser types
+export * from "./parsers/bill.parser";
+export * from "./parsers/grade-grid.parser";
+export * from "./parsers/hours-grid.parser";
